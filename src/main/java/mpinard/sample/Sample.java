165fb6c56089ff922e5b8b3c651b16ef20a24f84
Michael Pinard <michael.pinard@adp.com>
2025-03-11 09:13:58 -0400

    Sample with virtual theads and structured concurrency

--- src/main/java/mpinard/sample/Sample.java
+++ src/main/java/mpinard/sample/Sample.java
@@ -1,8 +1,46 @@
 package mpinard.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
+import java.util.random.RandomGenerator;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Sample {
 
-    public int calculateSum(int x, int y) {
-        return x + y;
+    public static void main(String[] args) throws InterruptedException {
+        log.info("Starting sample");
+        Sample sample = new Sample();
+        sample.execute();
+        log.info("Ending sample");
+    }
+
+    public void execute() throws InterruptedException {
+        List<StructuredTaskScope.Subtask<Results>> tasks = new ArrayList<>();
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            for (int i = 0; i < 5000; i++) {
+                final int each = i;
+                tasks.add(scope.fork(() -> runTask(each)));
+            }
+            scope.join().throwIfFailed();
+
+            tasks.forEach(task -> log.info("Result={}", task.get()));
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e.getCause());
+        }
+    }
+
+    private Results runTask(int i) throws InterruptedException {
+        long sleepLength = RandomGenerator.getDefault().nextLong(5000);
+        log.info("Starting task #{}", i);
+        Thread.sleep(sleepLength);
+        log.info("Ending task #{}", i);
+        return new Results(i, sleepLength);
+    }
+
+    private record Results(int index, long sleepLength) {
     }
 }
--- src/test/java/mpinard/sample/SampleTest.java
+++ src/test/java/mpinard/sample/SampleTest.java
@@ -8,7 +8,7 @@ public class SampleTest {
 
     @Test
     public void When_CalculateSum_Expect_Total() {
-        assertThat(new Sample().calculateSum(1, 2)).isEqualTo(3);
+//        assertThat(new Sample().calculateSum(1, 2)).isEqualTo(3);
     }
 
 }
